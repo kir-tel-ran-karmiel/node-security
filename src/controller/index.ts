c4227e1a9fd3059427cdcad55cbbcf391972414c
MetaL Kir <masterzatk@gmail.com>
2026-01-12 11:53:34 +0200

    demo expired token

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -4,12 +4,15 @@ import { JwtPayload } from "jsonwebtoken";
 
 
 const token = JwtUtil.getJwt("vasya12345", "USER");
-const corruptedToken = token.slice(0, 20) + 'g' + token.slice(21);
+setTimeout(() => verificationToken(token), 60000)
 
-try {
-    const payload: JwtPayload = JwtUtil.verifyToken(corruptedToken);
-    console.log(payload);
-} catch (error) {
-    console.log(error.name);
-    console.log(error.message);
+function verificationToken(token: string) {
+    try {
+        const payload: JwtPayload = JwtUtil.verifyToken(token);
+        console.log(payload);
+    } catch (error) {
+        console.log(error.name);
+        console.log(error.message);
+    }
 }
+
--- src/security/JwtUtil.ts
+++ src/security/JwtUtil.ts
@@ -1,12 +1,14 @@
-import jwt, {JwtPayload} from "jsonwebtoken"
+import jwt, { JwtPayload } from "jsonwebtoken"
 
 
 export default class JwtUtil {
     static getJwt(username: string, role: string): string {
+        const expiresIn: any = process.env.TOKEN_EXPIRED_IN || "1h";
         return jwt.sign(
-            {role}, 
-            process.env.JWT_SECRET, 
-            {subject: username},
+            { role },
+            process.env.JWT_SECRET,
+            { subject: username, expiresIn },
+
         )
     }
 
